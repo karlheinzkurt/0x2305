35554e19dabf5f00531e115b71119284a2d60b64
Sascha Lumma <sascha.lumma@deutschebahn.com>
2023-04-04 21:14:03 +0200

    Use apt-get instead of apt; Add clean; Upgrde 2 newest k3s version

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh pi@k8s1 'sudo apt -y update && sudo apt -y full-upgrade'
-ssh pi@k8s2 'sudo apt -y update && sudo apt -y full-upgrade'
-ssh pi@k8s3 'sudo apt -y update && sudo apt -y full-upgrade'
-ssh pi@k8s4 'sudo apt -y update && sudo apt -y full-upgrade'
+ssh pi@k8s1 'sudo apt-get -y update && sudo apt-get -y full-upgrade; sudo apt-get autoremove --purge; sudo apt-get clean'
+ssh pi@k8s2 'sudo apt-get -y update && sudo apt-get -y full-upgrade; sudo apt-get autoremove --purge; sudo apt-get clean'
+ssh pi@k8s3 'sudo apt-get -y update && sudo apt-get -y full-upgrade; sudo apt-get autoremove --purge; sudo apt-get clean'
+ssh pi@k8s4 'sudo apt-get -y update && sudo apt-get -y full-upgrade; sudo apt-get autoremove --purge; sudo apt-get clean'
--- bin/upgrade_k3s.sh
+++ bin/upgrade_k3s.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-VERSION=v1.22.3+k3s1
-k3sup install --ip 192.168.23.10 --user pi --k3s-version $VERSION
-k3sup join    --ip 192.168.23.20 --server-ip 192.168.23.10 --user pi --k3s-version $VERSION
-k3sup join    --ip 192.168.23.30 --server-ip 192.168.23.10 --user pi --k3s-version $VERSION
-k3sup join    --ip 192.168.23.40 --server-ip 192.168.23.10 --user pi --k3s-version $VERSION
-
+VERSION=v1.26.3+k3s1
+k3sup install --ip 192.168.178.210 --user pi --k3s-version $VERSION
+k3sup join    --ip 192.168.178.220 --server-ip 192.168.178.210 --user pi --k3s-version $VERSION
+k3sup join    --ip 192.168.178.230 --server-ip 192.168.178.210 --user pi --k3s-version $VERSION
+k3sup join    --ip 192.168.178.240 --server-ip 192.168.178.210 --user pi --k3s-version $VERSION
